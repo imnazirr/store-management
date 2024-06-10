79bb0490abb70cc253a6c56a9363a798a3716dfb
Imran Nazir <94141206+imnazirr@users.noreply.github.com>
2024-06-10 22:13:11 +0000

    Main added

---
+++ main.py
@@ -0,0 +1,9 @@
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+item1 = Item("Phone", 100)
+item1.name = "Phone"
+item1.price = 100
+item1.quantity = 5
